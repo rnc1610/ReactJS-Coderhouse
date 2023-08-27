e32a21f507f42fc0a5fe9677dce24b0aa71b74fc
rnc1610 <rodrigocastro1080@gmail.com>
2023-08-27 13:36:06 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import NavBar from './components/NavBar'
+import ItemListContainer from './components/itemListContainer'
 
 const App = () => {
+
+  const greeting = "Bienvenidos a Muebles Barcelona"
+
   return (
-    <div>App</div>
+    <>
+      <NavBar />
+      <ItemListContainer greeting={greeting} />
+
+    </>
   )
 }
 
