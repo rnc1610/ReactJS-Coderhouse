414d2915c9462105ef555638ba5ff9236db2b228
rnc1610 <rodrigocastro1080@gmail.com>
2023-08-27 13:20:32 +0200

    t

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
-import MiPrimerComponente from "./components/MiPrimerComponente";
-import { useState } from "react";
+import React from 'react'
+
 const App = () => {
-/*Lo de abajo son estados/hooks */
-  const [contador,setContador] = useState("0")
-  const [mensaje, setMensaje] = useState ("Mensaje inicial")
-  const [data, setData] = useState ([])
-/*Lo de abajo son props */
-  const curso = "React JS"
-  const greeting = "Bienvenido"
-
-return (
-    
-<>
-
-<h1>{contador}</h1>
-<button onClick={() => setContador(contador + 1)}>Sumar</button>
-<button onClick={() => setContador(contador - 1)}>Restar</button>
-<h2>{mensaje}</h2>
-<button onClick={() => setMensaje("Mensaje modificado")}>Cambiar Mensaje</button>
-<button onClick={() => setMensaje("Mensaje inicial") }>Revertir Mensaje</button>
-
- <h1>APP.jsx</h1>
-
- <MiPrimerComponente
- curso={curso}
- usuario={true}
- greeting={greeting}
-
- />
-
-</>  
+  return (
+    <div>App</div>
   )
 }
 
---
+++ src/components/cartWidget.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CartWidget = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default CartWidget
