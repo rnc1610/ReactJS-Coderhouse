2b4dee9744e2d2c51ae069f9223c878139d8fcfa
rnc1610 <rodrigocastro1080@gmail.com>
2023-09-10 17:13:29 +0200

    Ddd

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,61 +1,60 @@
 import React, {useState} from 'react'
 import {Card, CardBody, Image, Stack, Heading, Divider, Text, CardFooter, ButtonGroup, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter} from '@chakra-ui/react'
 import ItemDetail from './ItemDetail'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import '../../css/estilos.css'
 
-const Item = ({id, name, price, stock, image, description, longDescription}) => {
+function Item({ name, price, stock, image, description, longDescription }) {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const handleOpenModal = () => {
         setIsModalOpen(true);
-      };
-  return (
-    <div>
-    <Card maxW='sm'>
-        <CardBody>
-            <Image
-            src={image}
-            alt='Green double couch with wooden legs'
-            borderRadius='lg'
-            />
-            <Stack mt='6' spacing='3'>
-            <Heading size='md'>{name}</Heading>
-            <Text>
-                {description}
-            </Text>
-            <Text color='blue.600' fontSize='2xl'>
-                ${price}
-            </Text>
-            <Text color='blue.600' fontSize='2xl'>
-                Stock disponible: {stock} unidades
-            </Text>
-            </Stack>
-        </CardBody>
-        <Divider />
-        <CardFooter>
-            <ButtonGroup spacing='2'>
+    };
+    return (
+        <div className="product-container">
+          <Card className="Card" maxW='sm'>
+            <CardBody>
+              <Divider />
+              <Image
+                src={image}
+                alt='foto'
+                borderRadius='lg'
+              />
+              <Stack mt='6' spacing='3'>
+                <Heading size='md'>{name}</Heading>
+                <Text>{description}</Text>
+                <Text color='blue.600' fontSize='2xl'>
+                  ${price}
+                </Text>
+                <Text color='blue.600' fontSize='2xl'>
+                  Stock disponible: {stock} unidades
+                </Text>
+              </Stack>
+            </CardBody>
+            <CardFooter>
+              <ButtonGroup spacing='2'>
                 <Button variant='solid' colorScheme='blue' onClick={handleOpenModal}>
-                Detalles
+                  Detalles
                 </Button>
-            </ButtonGroup>
-            {isModalOpen && (
+              </ButtonGroup>
+              {isModalOpen && (
                 <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-                <ModalOverlay />
-                <ModalContent>
+                  <ModalOverlay />
+                  <ModalContent>
                     <ModalHeader>Detalles</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
-                    <ItemDetail name={name} price={price} image={image} longDescription={longDescription} />
+                      <ItemDetail name={name} price={price} image={image} longDescription={longDescription} />
                     </ModalBody>
                     <ModalFooter>
-                    <Button onClick={() => setIsModalOpen(false)}>Cerrar</Button>
+                      <Button onClick={() => setIsModalOpen(false)}>Cerrar</Button>
                     </ModalFooter>
-                </ModalContent>
-        </Modal>
-        )}
-        </CardFooter>
-        </Card>
-    </div>
-  )
-}
-
-export default Item
+                  </ModalContent>
+                </Modal>
+              )}
+            </CardFooter>
+          </Card>
+        </div>
+      );
+    }
+    
+    export default Item;
